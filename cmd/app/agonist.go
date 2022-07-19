47ea1011cbe63d37044da943d5a3fa6d121074cb
Constantine Zavezeon (Kwynto) <kwynto@mail.ru>
2022-07-19 19:36:20 +0300

    blank code

---
+++ cmd/app/agonist.go
@@ -0,0 +1,32 @@
+package app
+
+// blank code
+
+import (
+	"image/color"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+)
+
+func Run() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Canvas")
+	myCanvas := myWindow.Canvas()
+
+	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
+	rect := canvas.NewRectangle(blue)
+	myCanvas.SetContent(rect)
+
+	go func() {
+		time.Sleep(time.Second)
+		green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+		rect.FillColor = green
+		rect.Refresh()
+	}()
+
+	myWindow.Resize(fyne.NewSize(100, 100))
+	myWindow.ShowAndRun()
+}
--- main.go
+++ main.go
@@ -1,32 +1,9 @@
 package main
 
-// standart blank from Fyne
-
 import (
-	"image/color"
-	"time"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
+	"github.com/Kwynto/agonist/cmd/app"
 )
 
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Canvas")
-	myCanvas := myWindow.Canvas()
-
-	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
-	rect := canvas.NewRectangle(blue)
-	myCanvas.SetContent(rect)
-
-	go func() {
-		time.Sleep(time.Second)
-		green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
-		rect.FillColor = green
-		rect.Refresh()
-	}()
-
-	myWindow.Resize(fyne.NewSize(100, 100))
-	myWindow.ShowAndRun()
+	app.Run()
 }
