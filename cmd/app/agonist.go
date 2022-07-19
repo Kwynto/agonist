a89b06a113c58fd927df68eef5bbca1e240df4cf
Constantine Zavezeon (Kwynto) <kwynto@mail.ru>
2022-07-19 19:59:58 +0300

    blank code for [AGo]nist

--- cmd/app/agonist.go
+++ cmd/app/agonist.go
@@ -1,10 +1,9 @@
 package app
 
-// blank code
+// blank code for [AGo]nist
 
 import (
 	"image/color"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,21 +11,18 @@ import (
 )
 
 func Run() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Canvas")
-	myCanvas := myWindow.Canvas()
+	agonistApp := app.New()
+	agonistMainWindow := agonistApp.NewWindow("[AGo]nist")
+	agonistMainWindow.Resize(fyne.NewSize(600, 450))
+
+	agonistIconFile, _ := fyne.LoadResourceFromPath("icon.png")
+	agonistMainWindow.SetIcon(agonistIconFile)
+
+	agonistCanvas := agonistMainWindow.Canvas()
 
 	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
 	rect := canvas.NewRectangle(blue)
-	myCanvas.SetContent(rect)
 
-	go func() {
-		time.Sleep(time.Second)
-		green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
-		rect.FillColor = green
-		rect.Refresh()
-	}()
-
-	myWindow.Resize(fyne.NewSize(100, 100))
-	myWindow.ShowAndRun()
+	agonistCanvas.SetContent(rect)
+	agonistMainWindow.ShowAndRun()
 }
