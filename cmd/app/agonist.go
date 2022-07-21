a545d08016aa2aa012520639a065fa0d4220ebb2
Constantine Zavezeon (Kwynto) <kwynto@mail.ru>
2022-07-21 23:34:05 +0300

    added handlers.go and types.go

--- cmd/app/agonist.go
+++ cmd/app/agonist.go
@@ -7,27 +7,6 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-type agonistApp struct {
-	app        fyne.App
-	mainWindow fyne.Window
-
-	blankLabel *widget.Label
-
-	settingsCard,
-	alphabetCard,
-	superannuateCard,
-	aboutCard *widget.Card
-
-	homeBtn,
-	alphabetBtn,
-	superannuateBtn,
-	aboutBtn *widget.Button
-
-	mainHBox,
-	mainMenuBox,
-	workSpace *fyne.Container
-}
-
 var appAgonist agonistApp
 
 func (a *agonistApp) NewApplication() {
@@ -41,52 +20,32 @@ func (a *agonistApp) NewApplication() {
 	}
 	a.mainWindow.Resize(fyne.NewSize(600, 450))
 
-	a.blankLabel = widget.NewLabel("Blank label Blank label Blank label Blank label Blank label")
+	a.winElem.blankLabel = widget.NewLabel("Blank label Blank label Blank label Blank label Blank label")
 
-	a.settingsCard = widget.NewCard("Settings", "You need to fill in and save the settings for the further correct operation of the program.", a.blankLabel)
-	a.alphabetCard = widget.NewCard("Checking alphabetical order.", "Here you can check if the package list is in alphabetical order.", a.blankLabel)
-	a.superannuateCard = widget.NewCard("Checking for outdated packages.", "A package is considered obsolete if it has not been updated for more than a year.", a.blankLabel)
-	a.aboutCard = widget.NewCard("About.", "Information about the program, author and external components.", a.blankLabel)
+	a.winElem.settingsCard = widget.NewCard("Settings", "You need to fill in and save the settings for the further correct operation of the program.", a.winElem.blankLabel)
+	a.winElem.alphabetCard = widget.NewCard("Checking alphabetical order.", "Here you can check if the package list is in alphabetical order.", a.winElem.blankLabel)
+	a.winElem.superannuateCard = widget.NewCard("Checking for outdated packages.", "A package is considered obsolete if it has not been updated for more than a year.", a.winElem.blankLabel)
+	a.winElem.aboutCard = widget.NewCard("About.", "Information about the program, author and external components.", a.winElem.blankLabel)
 
-	a.workSpace = container.NewVBox(a.settingsCard, a.alphabetCard, a.superannuateCard, a.aboutCard)
+	a.workSpace = container.NewVBox(a.winElem.settingsCard, a.winElem.alphabetCard, a.winElem.superannuateCard, a.winElem.aboutCard)
 
 	{
 		icon, _ := fyne.LoadResourceFromPath("./static/img/icon-home-64.png")
-		a.homeBtn = widget.NewButtonWithIcon("", icon, func() {
-			a.settingsCard.Show()
-			a.alphabetCard.Hide()
-			a.superannuateCard.Hide()
-			a.aboutCard.Hide()
-		})
+		a.winElem.homeBtn = widget.NewButtonWithIcon("", icon, a.homeBtn())
 	}
 	{
 		icon, _ := fyne.LoadResourceFromPath("./static/img/icon-info-64.png")
-		a.alphabetBtn = widget.NewButtonWithIcon("", icon, func() {
-			a.settingsCard.Hide()
-			a.alphabetCard.Show()
-			a.superannuateCard.Hide()
-			a.aboutCard.Hide()
-		})
+		a.winElem.alphabetBtn = widget.NewButtonWithIcon("", icon, a.alphabetBtn())
 	}
 	{
 		icon, _ := fyne.LoadResourceFromPath("./static/img/icon-clock-64.png")
-		a.superannuateBtn = widget.NewButtonWithIcon("", icon, func() {
-			a.settingsCard.Hide()
-			a.alphabetCard.Hide()
-			a.superannuateCard.Show()
-			a.aboutCard.Hide()
-		})
+		a.winElem.superannuateBtn = widget.NewButtonWithIcon("", icon, a.superannuateBtn())
 	}
 	{
 		icon, _ := fyne.LoadResourceFromPath("./static/img/icon-about-64.png")
-		a.aboutBtn = widget.NewButtonWithIcon("", icon, func() {
-			a.settingsCard.Hide()
-			a.alphabetCard.Hide()
-			a.superannuateCard.Hide()
-			a.aboutCard.Show()
-		})
+		a.winElem.aboutBtn = widget.NewButtonWithIcon("", icon, a.aboutBtn())
 	}
-	a.mainMenuBox = container.NewVBox(a.homeBtn, a.alphabetBtn, a.superannuateBtn, a.aboutBtn)
+	a.mainMenuBox = container.NewVBox(a.winElem.homeBtn, a.winElem.alphabetBtn, a.winElem.superannuateBtn, a.winElem.aboutBtn)
 
 	a.mainHBox = container.NewHBox(a.mainMenuBox, a.workSpace)
 
@@ -96,10 +55,10 @@ func (a *agonistApp) NewApplication() {
 func Run() {
 	appAgonist.NewApplication()
 
-	appAgonist.settingsCard.Show()
-	appAgonist.alphabetCard.Hide()
-	appAgonist.superannuateCard.Hide()
-	appAgonist.aboutCard.Hide()
+	appAgonist.winElem.settingsCard.Show()
+	appAgonist.winElem.alphabetCard.Hide()
+	appAgonist.winElem.superannuateCard.Hide()
+	appAgonist.winElem.aboutCard.Hide()
 
 	appAgonist.mainWindow.ShowAndRun()
 }
---
+++ cmd/app/types.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+type agonistApp struct {
+	app        fyne.App
+	mainWindow fyne.Window
+
+	winElem agonistElements
+
+	mainHBox,
+	mainMenuBox,
+	workSpace *fyne.Container
+}
+
+type agonistElements struct {
+	blankLabel *widget.Label
+
+	settingsCard,
+	alphabetCard,
+	superannuateCard,
+	aboutCard *widget.Card
+
+	homeBtn,
+	alphabetBtn,
+	superannuateBtn,
+	aboutBtn *widget.Button
+}
